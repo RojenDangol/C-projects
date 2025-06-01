c4ca070e658e4320546bf48de1c582458beb45a6
Rojen Dangol <rojendangol44@gmail.com>
2025-06-01 17:48:36 +0545

    Practice

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    bool isRaining = false;
+    std::string name = "Rojen";
+    std::cout << "Hello "<< name << std::endl;
     std::cout << "It's a beautiful day!";
+    std::cout << (isRaining ? " It is raining." : " It is not raining.") << std::endl;
     return 0;
 }
