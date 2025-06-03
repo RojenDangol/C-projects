57139670f69c2b794adf2e60b864962977113ad1
Rojen Dangol <rojendangol44@gmail.com>
2025-06-03 17:00:21 +0545

    Initialize Stove temperature through constructor and update main function

--- getterSetter.cpp
+++ getterSetter.cpp
@@ -4,6 +4,10 @@ class Stove{
     private:
         int temperature = 0;
     public:
+
+    Stove(int temperature){
+        setTemperature(temperature); // Using setter to initialize temperature
+    }
         
     int getTemperature() {
         return temperature;
@@ -21,10 +25,10 @@ class Stove{
 };
 
 int main(){
-    Stove stove;
+    Stove stove(6);
 
     // stove.temperature = 200; // Setting the temperature of the stove
-    stove.setTemperature(5); // Using setter to set the temperature of the stove
+    // stove.setTemperature(5); // Using setter to set the temperature of the stove
 
     std::cout << "Stove temperature: " << stove.getTemperature() << " degrees." << std::endl;
     return 0;
