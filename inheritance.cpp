2dba0fd7f342d671d8a754a2cf6bd6ce777c4d55
Rojen Dangol <rojendangol44@gmail.com>
2025-06-03 17:13:26 +0545

    inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+class Animal{
+    public:
+        bool isAlive = true;
+
+    void eat(){
+        std::cout << "This animal is eating." << std::endl;
+    }
+};
+
+class Dog : public Animal{
+    public:
+    void bark(){
+        std::cout << "The dog is barking." << std::endl;
+    }
+};
+
+class Cat : public Animal{
+    public:
+    void meow(){
+        std::cout << "The cat is meowing." << std::endl;
+    }
+};
+
+
+
+int main(){
+    Dog dog;
+    Cat cat;
+
+    dog.eat(); 
+    dog.bark();
+    std::cout << "Is the dog alive? " << (dog.isAlive ? "Yes" : "No") << std::endl;
+
+    cat.eat();
+    cat.meow();
+    std::cout << "Is the cat alive? " << (cat.isAlive ? "Yes" : "No") << std::endl;
+    return 0;
+}
